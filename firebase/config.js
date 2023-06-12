2ff172b42db7ec53b64c236c6fd3eb274d85d910
MarlonChi <marlonchiodelli@hotmail.com>
2023-06-12 10:34:01 -0300

    :wrench: Firebase Config

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdLwVnekepjIXR5BqrR7tI3fIsiO4yEDc",
+  authDomain: "miniblog-79202.firebaseapp.com",
+  projectId: "miniblog-79202",
+  storageBucket: "miniblog-79202.appspot.com",
+  messagingSenderId: "220134335618",
+  appId: "1:220134335618:web:23ccd981adb59da1b3c09b",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getGetFirestore(app);
+
+export { db };
