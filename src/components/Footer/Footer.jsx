8dc6aa5f9dcd5370cec10e9e59ad239108df008d
MarlonChi <marlonchiodelli@hotmail.com>
2023-06-12 11:16:07 -0300

    :lipstick: Navbar, footer and project styles and fonts settings

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Footer.scss";
+
+export const Footer = () => {
+  return (
+    <footer className="Footer">
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini Blog &copy; 2023</p>
+    </footer>
+  );
+};
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./Navbar.scss";
+
+export const Navbar = () => {
+  return (
+    <nav className="Navbar">
+      <NavLink to="/" className="Navbar__brand">
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul className="Links">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Sobre
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
