d9da7b740b437407fb7eae8f9c2473f471fb572e
MarlonChi <marlonchiodelli@hotmail.com>
2023-06-12 10:44:05 -0300

    :wrench: React-Router-Dom and first pages base

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { Home } from "./pages/Home/Home";
+import { About } from "./pages/About/About";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
